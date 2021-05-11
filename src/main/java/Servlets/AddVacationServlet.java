fd855373603d814de5f024f49dcbab91b2beaa55
MiskaZupy <75077762+MiskaZupy@users.noreply.github.com>
2021-05-11 17:28:52 +0200

    Delete AddVacationServlet.java

--- src/main/java/Servlets/AddVacationServlet.java
+++
@@ -1,8 +0,0 @@
-package Servlets;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/VacationServlet")
-public class AddVacationServlet extends HttpServlet {
-}
